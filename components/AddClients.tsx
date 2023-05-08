d78a0e87e17da685efb48dcd25183bb2dec9476e
Sahil Satpute <explore.sahil@outlook.com>
2023-05-08 14:02:18 +0530

    updated

--- components/AddClients.tsx
+++ components/AddClients.tsx
@@ -61,7 +61,7 @@ function AddClients({ onClose }: Props) {
     }
   }
 
-  //*-----------------------------> Ai and Cdr files
+  //*----------------------------->
 
   const [selectedFiles, setSelectedFiles] = useState<FileWithPreview[]>([]);
   const imageInputRef = useRef<HTMLInputElement>(null);
@@ -92,12 +92,15 @@ function AddClients({ onClose }: Props) {
     );
   }
 
-  //*----------------------------->
+  //*----------------------------->  Ai and Cdr files
 
   const [selectedAiFiles, setSelectedAiFiles] = useState<File[]>([]);
   const [selectedCdrFiles, setSelectedCdrFiles] = useState<File[]>([]);
+  const [selectedOtherFiles, setSelectedOtherFiles] = useState<File[]>([]);
+
   const aiFileInputRef = useRef<HTMLInputElement>(null);
   const cdrFileInputRef = useRef<HTMLInputElement>(null);
+  const otherFileInputRef = useRef<HTMLInputElement>(null);
 
   const handleAiUploadButtonClick = () => {
     aiFileInputRef.current?.click();
@@ -105,6 +108,10 @@ function AddClients({ onClose }: Props) {
 
   const handleCdrUploadButtonClick = () => {
     cdrFileInputRef.current?.click();
+  };
+
+  const handleOtherUploadButtonClick = () => {
+    otherFileInputRef.current?.click();
   };
 
   return (
@@ -293,6 +300,136 @@ function AddClients({ onClose }: Props) {
                   onClick={() => {
                     setSelectedCdrFiles((prevSelectedCdrFiles) =>
                       prevSelectedCdrFiles.filter((_, i) => i !== index)
+                    );
+                  }}
+                  type="button"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="form-input upload-all-files-container">
+            <label htmlFor="#">Upload all logo image files:</label>
+            <input
+              type="file"
+              id="imageInput"
+              ref={imageInputRef}
+              style={{ display: "none" }}
+              accept=".jpg,.jpeg,.png"
+              multiple
+              onChange={handleImageInputChange}
+            />
+            <button
+              type="button"
+              className="input-button"
+              onClick={handleAddImagesButtonClick}
+            >
+              <BiCloudUpload size={25} />
+              Upload Files
+            </button>
+            {selectedFiles.length > 0 && (
+              <>
+                {selectedFiles.map((selectedFile) => (
+                  <div
+                    key={selectedFile.previewUrl}
+                    className="uploaded-all-files"
+                  >
+                    <span>{selectedFile.file.name}</span>
+                    <button
+                      type="button"
+                      className="input-button"
+                      onClick={() => handleClearButtonClickImages(selectedFile)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                ))}
+              </>
+            )}
+          </div>
+
+          <div className="form-input upload-all-files-container">
+            <label htmlFor="#">Upload .ai files:</label>
+            <input
+              type="file"
+              id="ai-upload"
+              multiple
+              accept=".ai"
+              onChange={(e) => {
+                if (e.target.files) {
+                  const filesArray = Array.from(e.target.files);
+                  setSelectedAiFiles((prevSelectedAiFiles) => [
+                    ...prevSelectedAiFiles,
+                    ...filesArray,
+                  ]);
+                }
+              }}
+              ref={aiFileInputRef}
+              style={{ display: "none" }}
+            />
+            <button
+              onClick={handleAiUploadButtonClick}
+              className="input-button"
+              type="button"
+            >
+              <BiCloudUpload size={25} />
+              Upload Files
+            </button>
+            {selectedAiFiles.map((file, index) => (
+              <div key={index} className="uploaded-all-files">
+                <p>{file.name}</p>
+
+                <button
+                  className="input-button"
+                  onClick={() => {
+                    setSelectedAiFiles((prevSelectedAiFiles) =>
+                      prevSelectedAiFiles.filter((_, i) => i !== index)
+                    );
+                  }}
+                  type="button"
+                >
+                  Remove
+                </button>
+              </div>
+            ))}
+          </div>
+          <div className="form-input upload-all-files-container">
+            <label htmlFor="#">Upload other files:</label>
+            <input
+              type="file"
+              id="other-upload"
+              multiple
+              accept="image/*, .cdr"
+              onChange={(e) => {
+                if (e.target.files) {
+                  const filesArray = Array.from(e.target.files);
+                  setSelectedCdrFiles((prevSelectedOtherFiles) => [
+                    ...prevSelectedOtherFiles,
+                    ...filesArray,
+                  ]);
+                }
+              }}
+              ref={otherFileInputRef}
+              style={{ display: "none" }}
+            />
+            <button
+              onClick={handleOtherUploadButtonClick}
+              className="input-button"
+              type="button"
+            >
+              <BiCloudUpload size={25} />
+              Upload Files
+            </button>
+            {selectedOtherFiles.map((file, index) => (
+              <div key={index} className="uploaded-all-files">
+                <p>{file.name}</p>
+
+                <button
+                  className="input-button"
+                  onClick={() => {
+                    setSelectedOtherFiles((prevSelectedOtherFiles) =>
+                      prevSelectedOtherFiles.filter((_, i) => i !== index)
                     );
                   }}
                   type="button"
